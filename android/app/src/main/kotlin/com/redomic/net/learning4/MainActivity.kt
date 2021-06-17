e6765d947c1c0b23bc6867a14e8f08da624d2ac8
Redomic <redomicmc@gmail.com>
2021-06-18 00:52:18 +0530

    First commit for shop app

---
+++ android/app/src/main/kotlin/com/redomic/net/learning4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.redomic.net.learning4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/products_grid.dart';
+
+enum FilterOptions {
+  Favorite,
+  All,
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorite) {
+                  _showOnlyFavorites = true;
+                } else {
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (ctx) => [
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: FilterOptions.Favorite,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.All,
+              ),
+            ],
+          ),
+        ],
+      ),
+      body: ProductsGrid(_showOnlyFavorites),
+    );
+  }
+}
