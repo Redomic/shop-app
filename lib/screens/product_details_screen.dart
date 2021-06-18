6c8080eea173c48996915b5424befc20c46ecdf1
Redomic <redomicmc@gmail.com>
2021-06-18 18:11:25 +0530

    Added details info and order floating button

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/products_provider.dart';
-
+import '../providers/cart.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   // final String title;
@@ -15,13 +15,51 @@ class ProductDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
 
     final String id = ModalRoute.of(context)!.settings.arguments as String;
-    final loadedProduct = Provider.of<Products>(context, listen: false).findById(id);
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(id);
+    final cart = Provider.of<Cart>(context, listen: false);
+
 
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(loadedProduct.imageUrl, fit: BoxFit.cover),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '\$${loadedProduct.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                loadedProduct.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add_shopping_cart),
+        onPressed: () {
+          cart.addItem(loadedProduct.id, loadedProduct.price, loadedProduct.title);
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
-
