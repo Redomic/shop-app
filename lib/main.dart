b320e2231c2078f24a37cd0bcaa913b93d7a4126
Redomic <redomicmc@gmail.com>
2021-06-19 17:56:54 +0530

    Added Edit products screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'screens/product_details_screen.dart';
 import 'screens/cart_screen.dart';
 import 'screens/orders_screen.dart';
 import 'screens/user_products_screen.dart';
+import 'screens/edit_product_screen.dart';
 
 // Providers
 import 'providers/products_provider.dart';
@@ -51,6 +52,7 @@ class MyApp extends StatelessWidget {
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
           UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+          EditProductScreen.routeName: (ctx) => EditProductScreen(),
         },
       ),
     );
--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -6,6 +6,8 @@ import '../providers/products_provider.dart';
 import '../widgets/user_product_item.dart';
 import '../widgets/app_drawer.dart';
 
+import '../screens/edit_product_screen.dart';
+
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
 
@@ -21,7 +23,7 @@ class UserProductsScreen extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.add),
             onPressed: () {
-
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
             },
           )
         ],
