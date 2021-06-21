fa9ad8e984725decf4eeed23b8fcc122da5c3dfd
Redomic <redomicmc@gmail.com>
2021-06-21 17:09:56 +0530

    add fetch DB data for product and overview loading screen

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -8,6 +8,7 @@ import '../widgets/app_drawer.dart';
 
 // Providers
 import '../providers/cart.dart';
+import '../providers/products_provider.dart';
 
 // Screens
 import '../screens/cart_screen.dart';
@@ -25,6 +26,25 @@ class ProductsOverviewScreen extends StatefulWidget {
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   var _showOnlyFavorites = false;
+  var _isInit = true;
+  var _isLoading = false;
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<Products>(context).fetchAndSetProducts()
+      .then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,7 +91,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
         ],
       ),
       drawer: AppDrawer(),
-      body: ProductsGrid(_showOnlyFavorites),
+      body: _isLoading ? Center(child: CircularProgressIndicator()) :ProductsGrid(_showOnlyFavorites),
     );
   }
 }
