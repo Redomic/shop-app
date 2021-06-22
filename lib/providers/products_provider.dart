148481d0f2063f9db04ee45298cea56d5c8d0b7d
Redomic <redomicmc@gmail.com>
2021-06-22 17:25:28 +0530

    add edit loading

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -124,9 +124,19 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(String id, Product newProduct) {
+  Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
+      final url = Uri.parse(
+          'https://shop-app-3be3b-default-rtdb.asia-southeast1.firebasedatabase.app/products/$id.json');
+      await http.patch(
+          url,
+          body: json.encode({
+            'title': newProduct.title,
+            'description': newProduct.description,
+            'imageUrl': newProduct.imageUrl,
+            'price': newProduct.price,
+      }));
       _items[prodIndex] = newProduct;
       notifyListeners();
     } else {
