4f6aea95bd641466ada2156797f5c7ed5b1d11c4
Redomic <redomicmc@gmail.com>
2021-06-23 23:04:15 +0530

    modify AuthScreen main body

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -6,14 +6,25 @@ import 'package:flutter/material.dart';
 enum AuthMode {Signup, Login}
 
 class AuthScreen extends StatelessWidget {
-  const AuthScreen({Key? key}) : super(key: key);
+  static const routeName = '/auth';
 
   @override
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
     return Scaffold(
       body: Stack(
-        children: <Widget>[],
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromRGBO(215, 117, 255, 1).withOpacity(0.5),
+                  Color.fromRGBO(215, 188, 117, 1).withOpacity(0.9)
+                ]
+              )
+            ),
+          ),
+        ],
       ),
     );
   }
