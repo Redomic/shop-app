32bb580092da960c4e5b792a5a5c1df74447cbf8
Redomic <redomicmc@gmail.com>
2021-06-23 22:59:32 +0530

    modify AuthScreen

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'dart:math';
+
+enum AuthMode {Signup, Login}
+
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
